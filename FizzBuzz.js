089db099de0feacc5e74a063544fb49cbaaa03cc
Carlos Cañado <ccanado@gmail.com>
2017-03-24 10:32:41 +0100

    Refactor function names

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,23 +1,24 @@
 'use strict';
-var fizzBuzz = function() {
+var printFizzBuzz = function() {
   for (var index = 0; index < 100; index++) {
-    console.log(translate(index));
+    console.log(fizzBuzz(index));
   }
 }
 
-var translate = function(value) {
-  if ( value%3 == 0 && value%5 == 0 ) {
+var fizzBuzz = function(number) {
+  if ( number%3 == 0 && number%5 == 0 ) {
     return 'FizzBuzz';
-  } else if ( value%3 == 0 ) {
+  } else if ( number%3 == 0 ) {
     return 'Fizz';
-  } else if ( value%5 == 0 ) {
+  } else if ( number%5 == 0 ) {
     return 'Buzz';
   } else {
-    return value;
+    return number;
   }
 }
 
-fizzBuzz();
+printFizzBuzz();
+
 
 
 // Tests
@@ -25,16 +26,16 @@ var assert = require('assert');
 
 describe('Test unit translations', function(){
   it('check a 3 multiple', function() {
-    assert.equal(translate(3), 'Fizz');
+    assert.equal(fizzBuzz(3), 'Fizz');
   });
   it('check a 5 multiple', function() {
-    assert.equal(translate(5), 'Buzz');
+    assert.equal(fizzBuzz(5), 'Buzz');
   });
   it('check a 3 and 5 multiple', function() {
-    assert.equal(translate(15), 'FizzBuzz');
+    assert.equal(fizzBuzz(15), 'FizzBuzz');
   });
   it('check a not 3 multiple nether 5 multiple', function() {
-    assert.equal(translate(4), 4);
+    assert.equal(fizzBuzz(4), 4);
   });
 
 });
