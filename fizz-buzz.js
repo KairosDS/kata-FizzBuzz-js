6865bd239d651ff1931951a6e2982a5e5d1230fb
Alberto Ortega <alberto.ortega.sanchez.contractor@bbva.com>
2017-03-24 10:26:41 +0100

    fizz-buzz multiplo de 3 y de 5

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -11,4 +11,10 @@ describe('Let try fizz-buzz', function() {
 	it('I have a function that return true when i give a number', function() {
 		assert.equal(fizzbuzz(3), true)
 	})
+	it('I have a function that return fizz when i give a number multiple of 3', function() {
+		assert.equal(fizzbuzz(3), 'fizz')
+	})
+	it('I have a function that return buzz when i give a number multiple of 5', function() {
+		assert.equal(fizzbuzz(5), 'buzz')
+	})
 })
