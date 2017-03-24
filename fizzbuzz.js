f6f0f7baef811e9df60bce4f4bfdb7a63186117a
Jesús García Martínez <jesus.garcia.martinez.contractor@bbva.com>
2017-03-24 10:41:32 +0100

    test

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,22 +1,24 @@
 'use strict';
 
-var fizzBuzz = () => {};
+var fizzBuzzPrinter = () => {};
 
-var fizzBuzzAux = (n) => {
+var fizzBuzz = n => {
+	if (n === 3) return 'Fizz'
 	return n
 };
 
 var assert = require('assert');
 describe('Debe existir la funcion fizzBuzz', function(){
 	it('Cuando comprobamos el tipo de la variable fizzBuzz , debe ser function', function(){
-		assert.equal(typeof fizzBuzz, 'function');
+		assert.equal(typeof fizzBuzzPrinter, 'function');
 	});
-	it('fizzBuzzAux should receive a number and return it', function(){
+	it('fizzBuzz should receive a number and return it', function(){
 		var number = 1;
-		assert.equal(fizzBuzzAux(number), number);
+		assert.equal(fizzBuzz(number), number);
 	});
-	it('fizzBuzzAux should receive a number multiple of three and it must return "Fizz"', function(){
+	it('fizzBuzz should receive a number multiple of three and it must return "Fizz"', function(){
 		var number = 3;
-		assert.equal(fizzBuzzAux(number), 'Fizz');
+		assert.equal(fizzBuzz(number), 'Fizz');
+		assert.equal(fizzBuzz(6), 'Fizz')
 	});
 });
