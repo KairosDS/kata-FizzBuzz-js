12e15ac3e46ecc0e9486289b5b63182280f53778
Carlos Cañado <ccanado@gmail.com>
2017-03-24 10:10:08 +0100

    First test failing: 3 multiple

---
+++ FizzBuzz.js
@@ -0,0 +1,20 @@
+'use strict';
+var fizzBuzz = function() {
+  for (var index = 0; index < 100; index++) {
+    console.log(translate(index));
+  }
+}
+
+var translate = function(value) {
+  return value;
+}
+
+
+// Tests
+var assert = require('assert');
+
+describe('Test unit translations', function(){
+  it('check a 3 multiple', function() {
+    assert.equal(translate(3), 'Fizz');
+  });
+});
