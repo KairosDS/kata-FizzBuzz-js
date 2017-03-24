652846ffb31ee9c5cc3d051abb44c4f00eedfe6c
Romulo Banares <romulo.banares@kairosds.com>
2017-03-24 10:33:53 +0100

    test fizzbug passed

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -2,6 +2,7 @@
 
 var fizzbuzz = function(number) {
 	if(typeof number === 'number') {
+		if(number % 3 === 0 && number % 5 === 0) return 'fizzbuzz'
 		if(number % 3 === 0) return 'fizz';
 		else if(number % 5 === 0) return 'buzz';
 		else return true;
