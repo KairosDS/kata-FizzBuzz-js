333f45432cc2ddc620f65e7af1af9bb7d331a263
Miguel Montes Martínez <miguel.montes@kairosds.com>
2017-03-24 10:57:53 +0100

    chore()

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,14 +3,15 @@
 var fizzBuzzPrinter = () => {};
 
 var fizzBuzz = n => {
-	if (n === 3) return 'Fizz'
+	if (n % 3 == 0) return 'Fizz'
 	return n
 };
 
 var assert = require('assert');
-describe('Debe existir la funcion fizzBuzz', function(){
-	it('Cuando comprobamos el tipo de la variable fizzBuzz , debe ser function', function(){
+describe('Debe existir la funcion fizzBuzz,', function(){
+	it('when checking typo of fizzBuzzPrinter and fizzBuzz , they must be function', function(){
 		assert.equal(typeof fizzBuzzPrinter, 'function');
+		assert.equal(typeof fizzBuzz, 'function');
 	});
 	it('fizzBuzz should receive a number and return it', function(){
 		var number = 1;
@@ -19,6 +20,10 @@ describe('Debe existir la funcion fizzBuzz', function(){
 	it('fizzBuzz should receive a number multiple of three and it must return "Fizz"', function(){
 		var number = 3;
 		assert.equal(fizzBuzz(number), 'Fizz');
-		assert.equal(fizzBuzz(6), 'Fizz')
+		assert.equal(fizzBuzz(6), 'Fizz');
+	});
+	it('fizzBuzz should receive a number multiple of five and it must return "Buzz"', function(){
+		var number = 5;
+		assert.equal(fizzBuzz(number), 'Buzz');
 	});
 });
