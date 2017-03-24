cc22ed5dc23640718327679b1f0cd9c203999b10
Romulo Banares <romulo.banares@kairosds.com>
2017-03-24 10:22:27 +0100

    test number passed

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,15 +1,13 @@
 'use strict';
 
 var fizzbuzz = function(name) {
-	//return;
+	if(typeof name === 'number') return true;
 }
 
 var assert = require('assert');
 
 describe('Let try fizz-buzz', function() {
-	it('I have a function that return true', function() {
-		assert.equal(fizzbuzz(), true);
-	})
+
 	it('I have a function that return true when i give a number', function() {
 		assert.equal(fizzbuzz(3), true)
 	})
