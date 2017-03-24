f7b235d64fa2621b619ba1412710af2fe4ee4857
Romulo Banares <romulo.banares@kairosds.com>
2017-03-24 10:30:00 +0100

    test print fizz buzz passed

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,7 +1,11 @@
 'use strict';
 
-var fizzbuzz = function(name) {
-	if(typeof name === 'number') return true;
+var fizzbuzz = function(number) {
+	if(typeof number === 'number') {
+		if(number % 3 === 0) return 'fizz';
+		else if(number % 5 === 0) return 'buzz';
+		else return true;
+	}
 }
 
 var assert = require('assert');
@@ -9,7 +13,7 @@ var assert = require('assert');
 describe('Let try fizz-buzz', function() {
 
 	it('I have a function that return true when i give a number', function() {
-		assert.equal(fizzbuzz(3), true)
+		assert.equal(fizzbuzz(4), true)
 	})
 	it('I have a function that return fizz when i give a number multiple of 3', function() {
 		assert.equal(fizzbuzz(3), 'fizz')
