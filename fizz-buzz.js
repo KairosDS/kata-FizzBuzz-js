6d92beba6b0bd51ab69c4b0f79ca335edaa99af3
Alberto Ortega <alberto.ortega.sanchez.contractor@bbva.com>
2017-03-24 10:19:13 +0100

    fizz-buzzo test con numero

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var fizzbuzz = function(name) {
-	return true;
+	//return;
 }
 
 var assert = require('assert');
@@ -9,5 +9,8 @@ var assert = require('assert');
 describe('Let try fizz-buzz', function() {
 	it('I have a function that return true', function() {
 		assert.equal(fizzbuzz(), true);
+	})
+	it('I have a function that return true when i give a number', function() {
+		assert.equal(fizzbuzz(3), true)
 	})
 })
